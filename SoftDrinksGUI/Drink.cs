331195d6927be216a69e70fb9e2a9f79d8a1a4b4
Kevin <arpaskevin11@gmail.com>
2024-10-04 14:05:00 +0200

    Add project files.

---
+++ SoftDrinksGUI/Drink.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftDrinksGUI
+{
+    internal class Drink
+    {
+        public Drink(string drinkName, string sweetener, int pricePerLiterInForint, string packageType, int fruitContentInPercentage, int packageUnit)
+        {
+            DrinkName = drinkName;
+            Sweetener = sweetener;
+            PricePerLiterInForint = pricePerLiterInForint;
+            PackageType = packageType;
+            FruitContentInPercentage = fruitContentInPercentage;
+            PackageUnit = packageUnit;
+        }
+
+        public string DrinkName { get; set; }
+        public string Sweetener { get; set; }
+        public int PricePerLiterInForint { get; set; }
+        public string PackageType { get; set; }
+        public int FruitContentInPercentage { get; set; }
+        public int PackageUnit { get; set; }
+    }
+}
---
+++ SoftDrinksGUI/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoftDrinksGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal List<Drink> drinks { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            drinks = new List<Drink>();
+
+            StreamReader srSoftDrinks = new(@"..\..\..\src\softDrinks.txt");
+
+            _ = srSoftDrinks.ReadLine();
+
+            while (!srSoftDrinks.EndOfStream)
+            {
+                var x = srSoftDrinks.ReadLine().Split(";");
+                drinks.Add(new(x[0], x[1], int.Parse(x[2]), x[3], int.Parse(x[4]), int.Parse(x[5])));
+            }
+
+            //4
+            Random r = new Random();
+
+            lblRecommendation.Content += $" {drinks[r.Next(drinks.Count)].DrinkName}";
+
+            //5
+
+            var f5 = drinks.Where(d => d.FruitContentInPercentage == 0).MinBy(d => d.PricePerLiterInForint);
+
+            lblCheapest.Content = $"{f5.DrinkName} - {f5.PricePerLiterInForint} Ft/l";
+
+            //6
+
+            lblHowManyManufacturersToChooseFrom.Content = $"{drinks.GroupBy(d => d.DrinkName.Split(" ")[0]).Count()} különböző termék típusunk van.";
+
+            //7
+
+            StreamWriter swAll = new(@"..\..\..\src\all.txt");
+
+            var f7 = drinks.GroupBy(d => d.DrinkName);
+
+            foreach (var item in f7)
+            {
+                swAll.WriteLine($"{item.Key} {Math.Round(item.Average(d => d.FruitContentInPercentage), 2)}");
+            }
+
+            //8
+
+            StreamWriter swSweetening = new(@"..\..\..\src\sweetening.txt");
+
+            var f8 = drinks.GroupBy(d => d.Sweetener);
+
+            foreach (var item in f8)
+            {
+                swSweetening.WriteLine($"{item.Key}-{item.Count()}");
+            }
+
+
+
+        }
+
+        private void btnBidSave_Click(object sender, RoutedEventArgs e)
+        {
+            string searchedDrinkName = textboxSearchedDrinkName.Text;
+
+            if (!string.IsNullOrWhiteSpace(searchedDrinkName) &&
+                drinks.Select(d => d.DrinkName.Split(" ")[0]).Contains(searchedDrinkName))
+            {
+                using StreamWriter swOffer = new(@"..\..\..\src\offer.txt", false);
+
+                var drinksMatchingUserInput = drinks.Where(d => d.DrinkName.Split(" ")[0] == searchedDrinkName).ToList();
+
+                foreach (var item in drinksMatchingUserInput)
+                {
+                    swOffer.WriteLine(item.DrinkName);
+                }
+
+                textboxSearchedDrinkName.Clear();
+
+                MessageBox.Show($"{drinksMatchingUserInput.Count} db termék íródott be a fájlba, melyeknek átlagára: {drinksMatchingUserInput.Average(d => d.PricePerLiterInForint)} Ft.");
+            }
+            else
+            {
+                MessageBox.Show("Nincs ilyen üdítőnk. Kérjük, válasszon mást!");
+            }
+        }
+
+        private void NewProductButtonClick(object sender, RoutedEventArgs e)
+        {
+            var newDrinkName = txtboxDrinkName.Text;
+            var newSweetener = txtboxSweetener.Text;
+            var newProductPrice = txtboxPrice.Text;
+            var newDrinkFruitContent = txtboxFruitContent.Text;
+            var newDrinkPackage = txtboxPackage.Text;
+
+
+            if (newDrinkName.Length == 0 || newSweetener.Length == 0 || newDrinkPackage.Length == 0
+                || !int.TryParse(newProductPrice, out var _)
+                || !int.TryParse(newDrinkFruitContent, out var _))
+            {
+                MessageBox.Show("Valamelyik adat hiányzik vagy nem helyes formátumú!", "Hiba");
+            }
+
+            else
+            {
+                using StreamWriter swSoftDrinks = new(@"..\..\..\src\softDrinks.txt", true);
+
+                swSoftDrinks.WriteLine($"{newDrinkName};{newSweetener};{newProductPrice};{newDrinkPackage};{newDrinkFruitContent};12");
+
+                MessageBox.Show("Az üdítő hozzáadásra került.");
+            }
+
+        }
+    }
+}
